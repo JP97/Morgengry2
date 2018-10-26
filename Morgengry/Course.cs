b9613aa17b6d09e613f3f5fbaac274b4e5916f51
Julian <juli11c0@edu.eal.dk>
2018-10-26 09:50:49 +0200

    private variables, constructor and properties

---
+++ Morgengry/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    public class Course
+    {
+        private string name;
+        private int durationinMinutes;
+
+        public Course(string name, int duration)
+        {
+            Name = name;
+            DurationinMinutes = duration;
+        }
+
+        public string Name { get {return name; } set {this.name = value; } }
+        public int DurationinMinutes { get {return this.durationinMinutes; } set {this.durationinMinutes = value; } }
+    }
+}
