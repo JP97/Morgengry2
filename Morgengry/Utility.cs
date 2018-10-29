de935954812bb1327659e5c79aa3ccf82b243cbd
Mia Pars Markussen <miax7855@edu.eal.dk>
2018-10-29 08:41:53 +0100

    lavet variabler og unit test

---
+++ Morgengry/Utility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class Utility
+    {
+
+        public double GetValueOfBook(double book)
+        {
+            return book;
+        }
+        public double GetValueOfAmulet(double amulet)
+        {
+
+        }
+        public double GetValueOfCourse(double course)
+        {
+            course = 875.00;
+            return course;
+        }
+    }
+}
