78cc045d3ecc5e230a28e818534c8760ef0d8de5
Mia Pars Markussen <miax7855@edu.eal.dk>
2018-10-29 10:30:12 +0100

    lavet 3 nye klasser

---
+++ Morgengry/CourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morgengry
+{
+    class CourseRepository
+    {
+        private List<Course> courses = new List<Course>();
+    }
+}
